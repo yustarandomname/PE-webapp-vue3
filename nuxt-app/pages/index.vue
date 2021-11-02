be98d05550ddca7c2bebf123ab75172ea455c74f
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-11-02 15:55:15 +0100

    Impl: grid

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -3,14 +3,21 @@
     <h1>Welcome to Nuxt.js!!!</h1>
     <Avatar>test</Avatar>
     <Button>test</Button>
+    <Grid>
+      <div>item1</div>
+      <div>item1</div>
+      <div>item1</div>
+      <div>item1</div>
+    </Grid>
   </div>
 </template>
 
 <script>
 import Avatar from "@/components/Avatar.vue";
 import Button from "@/components/Button.vue";
+import Grid from "@/components/Grid.vue";
 export default {
-  components: { Avatar, Button },
+  components: { Avatar, Button, Grid },
 };
 </script>
 
