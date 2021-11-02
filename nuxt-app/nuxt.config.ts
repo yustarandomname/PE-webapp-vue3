93e4645945448dded8cf93669da3661128f6a0cb
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-11-02 12:57:17 +0100

    Added: pwa support

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -2,4 +2,11 @@ import { defineNuxtConfig } from "nuxt3";
 
 export default defineNuxtConfig({
   css: ["~/assets/css/styles.scss"],
+  buildModules: ["@nuxtjs/pwa", "@nuxtjs/fontawesome"],
+  fontawesome: {
+    icons: {
+      solid: true,
+      brands: true,
+    },
+  },
 });
