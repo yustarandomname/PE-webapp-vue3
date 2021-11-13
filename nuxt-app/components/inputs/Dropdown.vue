4cc35e5b19a6d09ecac239bc3d91b1de75a87fe5
abel <abeldebruijn@icloud.com>
2021-11-13 21:39:17 +0100

    Added: dropdown

---
+++ nuxt-app/components/inputs/Dropdown.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="dropdown" :class="classes">
+    <div class="label">{{ label }}</div>
+    <select :disabled="disabled" v-model="input">
+      <option v-for="option in options" :key="option">{{ option }}</option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  name: "Dropdown",
+  props: {
+    label: {
+      type: String as PropType<string>,
+      default: "",
+    },
+    options: {
+      type: Array as PropType<string[]>,
+      default: () => [],
+    },
+    modelValue: {
+      type: String as PropType<string>,
+      default: "",
+    },
+    disabled: {
+      type: Boolean as PropType<boolean>,
+      default: false,
+    },
+    size: {
+      type: String as PropType<"small" | "medium" | "large">,
+      default: "medium",
+    },
+  },
+  setup(props, { emit }) {
+    const classes = computed(() => {
+      const classes: { [key: string]: boolean } = {};
+      classes["size-" + props.size] = !!props.size;
+      classes["disabled"] = props.disabled;
+      return classes;
+    });
+
+    const input = computed({
+      get() {
+        return props.modelValue as string;
+      },
+      set(value: string) {
+        emit("update:modelValue", value);
+      },
+    });
+
+    return {
+      classes,
+      input,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.dropdown {
+  position: relative;
+
+  & .label {
+    font-size: var(--small);
+  }
+
+  & select {
+    width: 20rem;
+    height: 2.2rem;
+    max-width: 90vw;
+    padding: 0.5rem 0.75rem;
+    border-radius: 0.25rem;
+    outline: none;
+    border: none;
+    background: var(--grey-color-200);
+    color: var(--grey-color-900);
+  }
+
+  &.disabled {
+    & .label {
+      color: var(--grey-color-500);
+    }
+  }
+}
+</style>
--- nuxt-app/pages/testing.vue
+++ nuxt-app/pages/testing.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <div class="container">
-      <Input placeholder="hello" v-model="input1" icon="trash-outline" />
+      <Input
+        placeholder="hello"
+        v-model="input1"
+        icon="trash-outline"
+        @iconClick="input1 = ''"
+      />
       <Checkbox v-model="input2" label="click me" />
+      <Dropdown v-model="input3" :options="['one', 'two', 'three']" />
       <p>input1: {{ input1 }}</p>
       <p>input2: {{ input2 }}</p>
-
-      <Avatar>testing</Avatar>
     </div>
   </div>
 </template>
@@ -14,16 +18,19 @@
 <script>
 import Input from "../components/inputs/Input.vue";
 import Checkbox from "../components/inputs/Checkbox.vue";
+import Dropdown from "../components/inputs/Dropdown.vue";
 
 export default {
   components: {
     Input,
     Checkbox,
+    Dropdown,
   },
   data() {
     return {
       input1: "123",
       input2: false,
+      input3: "",
     };
   },
 };
